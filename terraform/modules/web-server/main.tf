c499e9377cec5b32dd24848bb41f6164258cb710
Kevin Wong <twong358@my.bcit.ca>
2026-03-06 15:12:03 -0800

    main.tf changed to pull var

--- terraform/modules/web-server/main.tf
+++ terraform/modules/web-server/main.tf
@@ -1,11 +1,11 @@
 # create the ec2 instance
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ansible-name.id
-  instance_type          = "t3.micro"
-  key_name               = "aws-4640"
-  vpc_security_group_ids = [aws_security_group.web.id]
-  subnet_id              = aws_subnet.web.id
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = var.vpc_security_group_ids
+  subnet_id              = var.subnet_id
 
   tags = {
     Name = "Web"
