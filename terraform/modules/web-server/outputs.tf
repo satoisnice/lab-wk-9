0a21e182067dce55053829f6ab4158858731e1a4
satoisnice <connorbell015@gmail.com>
2026-03-06 14:58:46 -0800

    fixed output spelling

--- terraform/modules/web-server/outputs.tf
+++ terraform/modules/web-server/outputs.tf
@@ -1,4 +1,3 @@
-
 output "instance_ip_address" {
 	value	= aws_instance.web.public_ip
 	description = "Gets the public IP of the EC2"
@@ -7,7 +6,7 @@ output "instance dns name" {
 	value	= aws_instance.web.public_dns
 	description = "Gets the EC2 Public DNS"
 }
-out "instance id" {
+output "instance id" {
 	value	= aws_instance.web.id
 	description = "Gets the EC2 ID"
 }
