a5a5f91f139d68c292b36351da1cc76545244a98
Blu-Robin1 <omar.kaled2003@gmail.com>
2026-03-06 15:17:26 -0800

    module edit

--- terraform/main.tf
+++ terraform/main.tf
@@ -118,18 +118,12 @@ resource "aws_vpc_security_group_egress_rule" "web-egress" {
   ip_protocol = -1
 }
 
-# create the ec2 instance
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ansible-name.id
-  instance_type          = "t3.micro"
-  key_name               = "aws-4640"
-  vpc_security_group_ids = [aws_security_group.web.id]
-  subnet_id              = aws_subnet.web.id
+module "webserver" {
+	source = "./modules/web-server/"
 
-  tags = {
-    Name = "Web"
-  }
+	instance_name	= "web-server"
+	ami   = data.aws_ami.ansible-name.id
+	key_name = "lab_key"
 }
 
 # print public ip and dns to terminal
